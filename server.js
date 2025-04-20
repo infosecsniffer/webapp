7e826aa515315daa85a0976349e1a69951eaf622
infosecsniffer <cmiletti@opengatetech.com>
2025-04-20 14:29:32 -0400

    Update server.js to enable websockets

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const WebSocket = require('ws');
+
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('💀 XSS C2 server online. Awaiting payloads...\n');
+  res.end('💀 WebSocket XSS C2 active.');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', socket => {
+  console.log('[+] Client connected');
+  socket.on('message', msg => {
+    console.log(`[>] Data received: ${msg}`);
+  });
+
+  socket.send('🔥 Connected to XSS C2 server');
 });
 
 server.listen(PORT, () => {
